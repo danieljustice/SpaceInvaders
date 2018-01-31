19d1fbb7205839a5777907329d8562507bdebe10
Daniel Justice <danieljustice09@gmail.com>
2018-01-31 03:25:52 -0500

    movement works for aliens

---
+++ Assets/Scripts/AlienMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienMovement : MonoBehaviour {
+
+    public GameObject sprite1;
+    public GameObject sprite2;
+
+    public GridMovement gridMovement;
+	// Use this for initialization
+	void Start () {
+        sprite1.SetActive(true);
+        sprite2.SetActive(!sprite1.activeSelf);
+        gridMovement = GameObject.FindObjectOfType<GridMovement>();
+    }
+	
+
+    public void ChangeSprite()
+    {
+        sprite1.SetActive(!sprite1.activeSelf);
+        sprite2.SetActive(!sprite2.activeSelf);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collided");
+        if(collision.transform.tag == "Boundary")
+        {
+            gridMovement.ChangeDirection();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GridMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour {
+    public bool moveLeft = false;
+    public bool ableToChangeDirection = true;
+    public float stepRate = 1f;
+    public float stepDistance = 10f;
+    public float dropDistance = 10f;
+    private float deltaTime = 0f;
+    private AlienMovement[] alienMovements;
+    // Use this for initialization
+    void Start () {
+        alienMovements = transform.GetComponentsInChildren<AlienMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deltaTime += Time.deltaTime;
+        if(stepRate < deltaTime)
+        {
+            StepGrid();
+            FlipSprites();
+            deltaTime = 0f;
+            ableToChangeDirection = true;
+        }
+
+	}
+
+    public void FlipSprites()
+    {
+        for (int i = 0; i < alienMovements.Length; i++)
+        {
+            if (alienMovements[i].isActiveAndEnabled)
+            {
+                alienMovements[i].ChangeSprite();
+            }
+        }
+    }
+
+    public void StepGrid()
+    {
+        Vector3 step;
+        if (moveLeft)
+        {
+            step = new Vector3(-stepDistance, 0f, 0f);
+        }
+        else
+        {
+            step = new Vector3(stepDistance, 0f, 0f);
+        }
+
+        transform.position += step;
+
+    }
+
+    public void DropStep()
+    {
+        Vector3 step = new Vector3(0f, dropDistance, 0f);
+        transform.position -= step;
+    }
+    public void ChangeDirection()
+    {
+
+        if (ableToChangeDirection)
+        {
+            moveLeft = !moveLeft;
+            DropStep();
+        }
+        ableToChangeDirection = false;
+    }
+}
